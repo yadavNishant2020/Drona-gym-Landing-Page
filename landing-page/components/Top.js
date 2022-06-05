47244e4ba15edd68eb2ed18434fc3d1bc74152bc
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2022-06-05 23:06:50 +0530

    Setting background of intro page

---
+++ landing-page/components/Top.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Topcss from '../styles/css modules/top.module.css'
+function Top() {
+  return (
+    <>
+    <div className={Topcss.background}> </div>
+    <div className={Topcss.img}></div>
+    </>
+  )
+}
+
+export default Top
