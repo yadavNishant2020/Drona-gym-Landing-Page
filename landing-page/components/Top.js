1dcff3cf8b6391ea7c6e4d786c9c3c0f11cdded3
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2022-06-06 17:06:31 +0530

    adding button

--- landing-page/components/Top.js
+++ landing-page/components/Top.js
@@ -1,41 +1,55 @@
-import React from 'react'
-import Topcss from '../styles/css modules/top.module.css'
-import {Navbar, Container,Nav} from 'react-bootstrap'
+import React from "react";
+import Topcss from "../styles/css modules/top.module.css";
+import { Navbar, Container, Nav, Row, Col, Button } from "react-bootstrap";
 function Top() {
   return (
     <>
-     <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Container>
-  <Navbar.Brand href="#home">Sitara Fitness</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="#features">About</Nav.Link>
-      <Nav.Link href="#pricing">Facilities</Nav.Link>
-      <Nav.Link href="#pricing">Contact Us</Nav.Link>
-
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets"></Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-More      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
-    <div className={Topcss.background}>
-    <div className={Topcss.img}>
-        <img src="/img/intro.png"></img>
-    </div>
-
-    </div>
-    <div className={Topcss.heading}>
-        <p>BE <span className={Topcss.blk}>FIT</span>,<br/>LIVE <span className={Topcss.blk}>MORE</span></p>
-        <p></p>
-    </div>
-    
+    {/* Navbar */}
+      <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Sitara Fitness</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">About</Nav.Link>
+              <Nav.Link href="#pricing">Facilities</Nav.Link>
+              <Nav.Link href="#pricing">Contact Us</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets"></Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                More{" "}
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {/* background */}
+      <div className={Topcss.background}></div>
+      <Container className="container-top">
+        <Row className="section-top">
+          <Col xs={12} md={6} className="section-top-right">
+            <div className={Topcss.heading}>
+              <p>
+                BE <span className={Topcss.blk}>FIT,</span><br />
+                LIVE <span className={Topcss.blk}>MORE</span>
+              </p>
+              <div className="mb-2">
+    <Button className={Topcss.btn} variant="dark" size="lg">
+      JOIN NOW
+    </Button>
+  </div>
+            </div>
+          </Col>
+          <Col xs={12} md={6} className="section-top-left">
+            <img className={Topcss.img} src="/img/intro.png" />
+          </Col>
+          
+        </Row>
+      </Container>
+     
     </>
-  )
+  );
 }
 
-export default Top
+export default Top;
