50f4a6fe972c64a8864ae0b8956ce6ba448551ec
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2022-06-09 02:40:20 +0530

    Build and bebel error solved

--- landing-page/components/Top.js
+++ landing-page/components/Top.js
@@ -3,17 +3,17 @@ import Topcss from "../styles/css modules/top.module.css";
 import { Navbar, Container, Nav, Row, Col, Button } from "react-bootstrap";
 function Top() {
   return (
-    < div className={Topcss.back}>
-    {/* Navbar */}
+    <div className={Topcss.back}>
+      {/* Navbar */}
       <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">DRONA GYM</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Testimonials</Nav.Link>
+              <Nav.Link href="#pricing">About Us</Nav.Link>
               <Nav.Link href="#pricing">Facilities</Nav.Link>
-              <Nav.Link href="#pricing">Contact Us</Nav.Link>
+              <Nav.Link href="#features">Testimonials</Nav.Link>
             </Nav>
             <Nav>
               <Nav.Link href="#deets"></Nav.Link>
@@ -27,25 +27,21 @@ function Top() {
       {/* background */}
       <div className={Topcss.background}></div>
       <div className={Topcss.cont}>
-      <Container className="container-top">
-        <Row className="section-top">
-          <Col xs={12} md={6} className="section-top-right">
-            <div className={Topcss.heading}>
-              <p>
-              IT’S ALL ABOUT WHAT YOU CAN ACHIEVE
-              </p>
-              <div className="mb-2">
-    <Button className={Topcss.btn} variant="dark" size="lg">
-      JOIN NOW
-    </Button>
-  </div>
-            </div>
-          </Col>
-          <Col xs={12} md={6} className="section-top-left">
-          </Col>
-          
-        </Row>
-      </Container>
+        <Container className="container-top">
+          <Row className="section-top">
+            <Col xs={12} md={6} className="section-top-right">
+              <div className={Topcss.heading}>
+                <p>IT’S ALL ABOUT WHAT YOU CAN ACHIEVE</p>
+                <div className="mb-2">
+                  <Button className={Topcss.btn} variant="dark" size="lg">
+                    JOIN NOW
+                  </Button>
+                </div>
+              </div>
+            </Col>
+            <Col xs={12} md={6} className="section-top-left"></Col>
+          </Row>
+        </Container>
       </div>
     </div>
   );
